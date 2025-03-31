0fdb27b95bfa6f28fe722509e5809aeb2ca9ed8a
Andrey-Mikhailov-Fro <dark.ace.frost@gmail.com>
2025-03-31 06:59:21 +0300

    prettyfied

--- my-project/src/components/sidebar/Navbar.tsx
+++ my-project/src/components/sidebar/Navbar.tsx
@@ -32,10 +32,7 @@ function Navbar({ variant }: NavBarProps) {
     <div className="navigation">
       <div className="navigation-container">
         <span className="navigation-tag">{variants[variantKey].name}</span>
-        <button
-          className="navigation-dropdown"
-          onClick={() => setShow(!show)}
-        >
+        <button className="navigation-dropdown" onClick={() => setShow(!show)}>
           <img
             className={show ? "opened" : ""}
             src="/dropdown.svg"
@@ -47,7 +44,7 @@ function Navbar({ variant }: NavBarProps) {
         {variants[variantKey].list.map((item) => {
           const { imageId, text } = item;
           const itemIcon = variants[variantKey].icons.find(
-            (icon) => icon.id === imageId
+            (icon) => icon.id === imageId,
           );
 
           return (
